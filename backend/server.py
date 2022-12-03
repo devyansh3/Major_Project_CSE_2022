918701501db9745294e63f940dbfab253027de34
Hriday Pradhan <hridayjyotipradhandc@gmail.com>
2022-12-03 16:23:40 +0530

    Comment removal

--- backend/server.py
+++ backend/server.py
@@ -1,16 +1,13 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import datetime
 
 x = datetime.datetime.now()
 
-# Initializing flask app
 app = Flask(__name__)
 
 
-# Route for seeing a data
 @app.route('/data')
 def get_time():
-    # Returning an api for showing in  reactjs
     return {
         'Name': "geek",
         "Age": "22",
